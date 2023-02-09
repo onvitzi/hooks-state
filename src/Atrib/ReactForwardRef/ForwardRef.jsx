331e2a1ed6995cd6f218d2f918f1f988d830da56
jovanzabala <jovanzabala@gmail.com>
2023-02-09 13:32:50 -0500

    forwardref

---
+++ src/Atrib/ReactForwardRef/ForwardRef.jsx
@@ -0,0 +1,11 @@
+import { forwardRef } from 'react';
+
+export const MyInput = forwardRef(function MyInput(props, ref) {
+  const { label, ...otherProps } = props;
+  return (
+    <label>
+      {label}
+      <input {...otherProps} ref={ref} />
+    </label>
+  );
+});
