30bed23ac94a2a0da27dc5487b6b05dc00db9f5b
jovanzabala <jovanzabala@gmail.com>
2023-02-09 13:37:36 -0500

    text forwardRef

--- src/Atrib/ReactForwardRef/ForwardRef.jsx
+++ src/Atrib/ReactForwardRef/ForwardRef.jsx
@@ -9,3 +9,16 @@ export const MyInput = forwardRef(function MyInput(props, ref) {
     </label>
   );
 });
+
+// Uso
+// *Exponer un nodo DOM al componente padre
+// *Pasar una ref a través de múltiples componentes
+// *Exposición de un manejador imperativo en lugar de un nodo DOM
+// Solución de problemas
+// *Mi componente está envuelto en forwardRef, pero la ref a él es siempre null.
+
+// En el ejemplo anterior, React pasa un ref dado a un elemento <FancyButton ref={ref}> 
+// como un segundo argumento a la función de renderizado dentro de la llamada React.forwardRef. 
+// Esta función de renderizado pasa el ref al elemento <button ref={ref}>.
+
+// Como resultado, después que React adjunte el ref, ref.current apuntará directamente a la instancia del elemento DOM <button>.
