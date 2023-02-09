fafd75bfffd0a8cb1e3050241f0e78629225ac0e
jovanzabala <jovanzabala@gmail.com>
2023-02-09 10:06:10 -0500

    repositories hooks

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { CounterApp } from './01-useState/';
 
 function App() {
+ 
+  const list = [1, 5, 20] 
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+       <CounterApp/>
+    </>
+    // <div className="App">
+    //  {list.map((i) => (
+    //     <CounterApp/>
+    //   ))}
+    // </div>
   );
 }
 
